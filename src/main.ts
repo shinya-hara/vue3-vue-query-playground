a4e8d597b2bda5ee9f756b33f61d52868749458d
Shinya Hara <rabby.coffee.c5@gmail.com>
2022-10-16 23:26:39 +0900

    ✨ Todo一覧 vue-queryで表示してみる

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import App from './App.vue';
 import router from './router';
 import './assets/styles/app.scss';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(VueQueryPlugin).mount('#app');
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,44 @@
 <template>
-  <h1>@/pages/index.vue</h1>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <span v-if="isLoading">Loading...</span>
+  <span v-else-if="isError">Error: {{ error?.message }}</span>
+  <!-- We can assume by this point that `isSuccess === true` -->
+  <ul v-else>
+    <li v-for="todo in data" :key="todo.id">{{ todo.title }}</li>
+  </ul>
+  <button @click="onButtonClick">Add Todo</button>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue';
+import { useQueryClient, useQuery, useMutation } from '@tanstack/vue-query';
+import { CreateTodoDto, TodoRepositoryImpl } from '@/infra/todoRepositoryImpl';
+import { Todo } from '@/domain/entity/todo/todo';
+
+const todoRepository = new TodoRepositoryImpl();
+
+// Access QueryClient instance
+const queryClient = useQueryClient();
+
+// Query
+const { isLoading, isError, data, error } = useQuery<Todo[], Error>(
+  ['todos'],
+  () => todoRepository.findAll(),
+);
+
+// Mutation
+const mutation = useMutation<Todo, Error, CreateTodoDto>(
+  (newTodo) => todoRepository.create(newTodo),
+  {
+    onSuccess: () => {
+      // Invalidate and refetch
+      queryClient.invalidateQueries(['todos']);
+    },
+  },
+);
+
+const onButtonClick = () => {
+  mutation.mutate({
+    title: 'new Task!',
+    userId: 1,
+  });
+};
 </script>
